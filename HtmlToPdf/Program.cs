407d2edaf29a96761f8d33c828d938b5dbb91e7f
Anusha Ramadugu <ramaduguanusha123@gmail.com>
2025-04-06 09:17:58 +1000

    used ironpdf to convert html to pdf

--- HtmlToPdf/Program.cs
+++ HtmlToPdf/Program.cs
@@ -4,11 +4,14 @@ using Razor.Templating.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
+IronPdf.License.LicenseKey = builder.Configuration["IronPdf:LicenseKey"];
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddRazorTemplating();
+builder.Services.AddSingleton<InvoiceFactory>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,11 +26,13 @@ app.MapGet("invoice-report", async (InvoiceFactory invoiceFactory) =>
     Invoice invoice = invoiceFactory.CreateInvoice();
     
     var html = await RazorTemplateEngine.RenderAsync("Views/InvoiceReport.cshtml", invoice);
-    var htmlToPdf = new NReco.PdfGenerator.HtmlToPdfConverter();
-    var pdf = htmlToPdf.GeneratePdf(html);
-    
-    return Results.File(pdf, "application/pdf", $"invoice-{invoice.Number}.pdf");
 
+    var renderer = new ChromePdfRenderer();
+    var pdf = renderer.RenderHtmlAsPdf(html);
+    
+   // File.WriteAllBytes(outputPdfPath, pdf);
+    
+    return Results.File(pdf.BinaryData, "application/pdf", $"invoice-{invoice.Number}.pdf");
 });
 
 app.UseHttpsRedirection();
